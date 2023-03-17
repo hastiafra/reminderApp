5806f67a4c2cf520ad0bdf1038771301e9d156ad
hastiafra <77177165+hastiafra@users.noreply.github.com>
2023-03-17 15:35:01 -0400

    Added input and basic style

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [inputText, setInputText] = useState("");
+
+  const [listItems, setListItems] = useState([])
+
+  const inputValueHandler = (value) => {
+    setInputText(value)
+  };
+
+  const addButton = () => {
+    console.log(inputText)
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.reminderContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Type something!"
+          onChangeText={inputValueHandler}
+        />
+        <Button title="Add" onPress={addButton} />
+      </View>
+      <View style={styles.listContainer}>
+        <Text style={styles.listTitle}>Your List...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  reminderContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 40,
+    marginBottom: 10,
+    marginTop: 40,
+    alignItems: "center",
+  },
+  textInput: {
+    width: "80%",
+    borderColor: "#cccccc",
+    borderWidth: 1,
+    padding: 10,
+  },
+  listContainer: {
+    flex: 3,
+    paddingHorizontal: 40,
+  },
+  listTitle: {
+    textTransform: "uppercase",
+    fontWeight: 700,
   },
 });
